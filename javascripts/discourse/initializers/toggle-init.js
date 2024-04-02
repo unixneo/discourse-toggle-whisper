7b0d32126ec1504d207284f6debf6ab11a7ccfbf
unixneo <neo@unix.com>
2024-04-02 19:42:19 +0700

    Debug

--- javascripts/discourse/initializers/toggle-init.js
+++ javascripts/discourse/initializers/toggle-init.js
@@ -11,10 +11,7 @@ export default {
 
 const whisperInit = (api) => {
   const currentUser = api.getCurrentUser();
-  const myObj = {currentUser:currentUser, currentUser_staff:currentUser.staff, currentUser_whisperer: currentUser.whisperer };
-  console.log(myObj);
   if (currentUser && currentUser.staff && currentUser.whisperer) {
-    console.log("whisperInit is true");
     api.attachWidgetAction("post-menu", "toggleWhisper", function () {
       const model = this.attrs;
       let newType = model.post_type === 1 ? 4 : 1;
@@ -28,6 +25,8 @@ const whisperInit = (api) => {
     });
 
     api.addPostMenuButton("toggleWhisper", (model) => {
+      const myObj = {model_staff:model.staff, model_post_number:model.post_number };
+      console.log(myObj);
       if (!model.staff || model.post_number < 2) return;
 
       let isWhisper = model.post_type === 4;
